2fe79c9d8b7db36b4621fc819e9fbf380ddc75e7
Tim <57221600+TiltedToast@users.noreply.github.com>
2023-07-31 16:57:35 +0200

    cleanup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,22 @@
-import net.dv8tion.jda.api.{JDABuilder, JDA}
 import io.github.cdimascio.dotenv.Dotenv
-import net.dv8tion.jda.api.hooks.ListenerAdapter
-import net.dv8tion.jda.api.events.session.ReadyEvent
+import net.dv8tion.jda.api.{EmbedBuilder, JDABuilder}
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.events.session.ReadyEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.requests.GatewayIntent
-import net.dv8tion.jda.api.EmbedBuilder
-import net.dv8tion.jda.api.events.GenericEvent
-import net.dv8tion.jda.api.entities.Message
-import java.time.{Duration, LocalDateTime, Instant}
-import java.text.SimpleDateFormat
-import java.util.Date
 import org.slf4j.LoggerFactory
+
+import java.text.SimpleDateFormat
+import java.time.{Duration, Instant}
+import java.util.Date
 
 val dotenv = Dotenv.load()
 val now = Instant.now()
 val prefix = "h?"
-val logger = LoggerFactory.getLogger(Main.getClass())
+val logger = LoggerFactory.getLogger(Main.getClass)
 
 object Main extends App {
-    val jda = JDABuilder
+    private val jda = JDABuilder
         .createDefault(dotenv.get("BOT_TOKEN"))
         .enableIntents(
           GatewayIntent.GUILD_MESSAGES,
@@ -27,7 +25,7 @@ object Main extends App {
           GatewayIntent.MESSAGE_CONTENT
         )
         .build()
-    java.lang.Runtime.getRuntime().addShutdownHook(Thread(() => jda.shutdownNow()))
+    java.lang.Runtime.getRuntime.addShutdownHook(Thread(() => jda.shutdownNow()))
     jda.addEventListener(
       ReadyListener(),
       MsgListener()
@@ -37,42 +35,42 @@ object Main extends App {
 final class ReadyListener extends ListenerAdapter {
     override def onReady(event: ReadyEvent): Unit = {
         val readyTime = Instant.now()
-        val timeDiff = Duration.between(now, readyTime).toMillis() / 1000.0
+        val timeDiff = Duration.between(now, readyTime).toMillis / 1000.0
         val dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
         val formatted = dateFormat.format(Date.from(readyTime))
 
         logger.info(s"Started up in $timeDiff seconds on $formatted")
         logger.info("Logged in as:")
-        logger.info(event.getJDA().getSelfUser().getName())
-        logger.info(event.getJDA().getSelfUser().getId())
+        logger.info(event.getJDA.getSelfUser.getName)
+        logger.info(event.getJDA.getSelfUser.getId)
         logger.info("------------------")
     }
 }
 
 final class MsgListener extends ListenerAdapter {
     override def onMessageReceived(event: MessageReceivedEvent): Unit = {
-        if event.getAuthor().isBot() then return
-        val content = event.getMessage().getContentRaw().split(" ").toSeq
+        if event.getAuthor.isBot then return
+        val content = event.getMessage.getContentRaw.split(" ").toSeq
 
         if !content(0).startsWith(prefix) then return
 
         content(0).toLowerCase().stripPrefix(prefix) match {
-            case "ping" => event.getChannel().sendMessage("pong!").queue()
+            case "ping" => event.getChannel.sendMessage("pong!").queue()
             case "pfp" => {
                 val id = content
                     .lift(1)
-                    .getOrElse(event.getAuthor().getId())
+                    .getOrElse(event.getAuthor.getId)
 
-                val target = event.getJDA().retrieveUserById(id).complete()
+                val target = event.getJDA.retrieveUserById(id).complete()
 
-                val url = target.getEffectiveAvatarUrl() + "?size=4096"
+                val url = target.getEffectiveAvatarUrl + "?size=4096"
                 val embed = EmbedBuilder()
                     .appendDescription("*Here's your pfp!*")
                     .setImage(url)
                     .setColor(0xce3a9b)
                     .build()
 
-                event.getChannel().sendMessageEmbeds(embed).queue()
+                event.getChannel.sendMessageEmbeds(embed).queue()
             }
             case _ => ()
         }
