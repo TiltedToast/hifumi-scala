a3680e16ebd576bfbec61da1daf208fdfaa02141
Tim <57221600+TiltedToast@users.noreply.github.com>
2023-09-06 04:01:21 +0200

    cleanup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,20 @@
 import io.github.cdimascio.dotenv.Dotenv
-import net.dv8tion.jda.api.{EmbedBuilder, JDABuilder}
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.events.session.ReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.requests.GatewayIntent
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import java.text.SimpleDateFormat
-import java.time.{Duration, Instant}
+import java.time.Duration
+import java.time.Instant
 import java.util.Date
 
 val dotenv = Dotenv.load()
-val now = Instant.now()
+val startupTime = Instant.now()
 val prefix = "h?"
 val logger = LoggerFactory.getLogger(Main.getClass)
 
@@ -35,7 +38,7 @@ object Main extends App {
 final class ReadyListener extends ListenerAdapter {
     override def onReady(event: ReadyEvent): Unit = {
         val readyTime = Instant.now()
-        val timeDiff = Duration.between(now, readyTime).toMillis / 1000.0
+        val timeDiff = Duration.between(startupTime, readyTime).toMillis / 1000.0
         val dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
         val formatted = dateFormat.format(Date.from(readyTime))
 
@@ -52,11 +55,11 @@ final class MsgListener extends ListenerAdapter {
         if event.getAuthor.isBot then return
         val content = event.getMessage.getContentRaw.split(" ").toSeq
 
-        if !content(0).startsWith(prefix) then return
+        if !content.head.startsWith(prefix) then return
 
-        content(0).toLowerCase().stripPrefix(prefix) match {
+        content.head.toLowerCase().stripPrefix(prefix) match {
             case "ping" => event.getChannel.sendMessage("pong!").queue()
-            case "pfp" => {
+            case "pfp" =>
                 val id = content
                     .lift(1)
                     .getOrElse(event.getAuthor.getId)
@@ -71,7 +74,7 @@ final class MsgListener extends ListenerAdapter {
                     .build()
 
                 event.getChannel.sendMessageEmbeds(embed).queue()
-            }
+
             case _ => ()
         }
     }
